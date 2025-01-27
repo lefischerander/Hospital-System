71f8f73ac9202530e61a3eed9284a9e0079259f3
Kostja18 <konstantin.kolbek@stud.uni-goettingen.de>
2025-01-27 18:50:07 +0100

    update login
    Ich habe die funktion aktions als eine eigene Datei erstellt und in log in importiert

--- login_final.py
+++ login_final.py
@@ -1,6 +1,7 @@
 from user import admins, users  # Import user and admin data
 import getpass  # Import getpass for hidden password input
 import hashlib
+from actions import admin_actions, doktor_actions, patient_actions
 
 def login():
     # Prompt for username and password
@@ -27,80 +28,80 @@ def login():
     else:
         print("Invalid username or password!")  # Output if login data is incorrect
 
-def admin_actions():
+##def admin_actions():
     # Actions for the admin
-    print("Your possible actions: ")
-    print()
-    print("1. View all users")
-    print("\n1. View all users")
-    print("\n3. Logout")
-    print("\n4. Change password") ## added by Nantequiou
+##    print("Your possible actions: ")
+##    print()
+##    print("1. View all users")
+##    print("\n1. View all users")
+##    print("\n3. Logout")
+##    print("\n4. Change password") ## added by Nantequiou
 
     
-    choice = input("Choose an action: ")
-    if choice == '1':
-        print("Users: ", users) # Show all users
-        print()
-        admin_actions() # Return to admin actions
-    elif choice == '2':
-        username = input("Username: ")
-        user = users.get(username)  # Show specific user
-        print(user)
-        print()
-        admin_actions() # Return to admin actions
-    elif choice == '3':
-        logout()    # Logout
+#    choice = input("Choose an action: ")
+#    if choice == '1':
+#        print("Users: ", users) # Show all users
+#        print()
+#        admin_actions() # Return to admin actions
+#    elif choice == '2':
+#        username = input("Username: ")
+#        user = users.get(username)  # Show specific user
+#        print(user)
+#        print()
+#        admin_actions() # Return to admin actions
+#    elif choice == '3':
+#        logout()    # Logout
 
-def doktor_actions():
+#def doktor_actions():
     # Actions for the doctor
-    print("Your possible actions: ")
-    print("\n2. Logout")
-    print("\n3. Change password") ## added by Nantequiou
-    print()
-    
-    choice = input("Choose an action: ")
-    if choice == '1':
-        username = input("Patient: ")
-        patient = users.get(username)   # Show patient data
-        if patient:
-            if patient['role'] == 'patient':
-                print("Folder is empty")
-                print()
-            else:
-                print("User is not a patient!") # Check if user is a patient
-                print()
-        else:
-            print("Such patient doesn't exist!")    # Check if patient exists
-            print()
-        doktor_actions()    # Return to doctor actions
-
-    elif choice == '2':
-        logout()    # Logout
-
-        reset_password() ## added by Nantequiou
-
-        reset_password() ## added by Nantequiou
-
-def patient_actions():
-    # Actions for the patient
-    print("Your possible actions: ")
-    print("\n1. View your medical record")
-    print("\n3. Change password") ## added by Nantequiou
-    
-
-    print()
-    
-    choice = input("Choose an action: ")
-    if choice == '1':
-        print("Folder is empty")    # Show empty directory (no medical record)
-        print()
-        patient_actions()   # Return to patient actions
-    elif choice == '2':
-        logout()    # Logout
-    
-    elif choice == '3':
-        reset_password() ## added by Nantequiou
-
+##    print("Your possible actions: ")
+#    print("\n2. Logout")
+#    print("\n3. Change password") ## added by Nantequiou
+#    print()
+#    
+#    choice = input("Choose an action: ")
+#    if choice == '1':
+#        username = input("Patient: ")
+#        patient = users.get(username)   # Show patient data
+#        if patient:
+#            if patient['role'] == 'patient':
+#                print("Folder is empty")
+#                print()
+#            else:
+#                print("User is not a patient!") # Check if user is a patient
+#                print()
+#        else:
+#            print("Such patient doesn't exist!")    # Check if patient exists
+#            print()
+#        doktor_actions()    # Return to doctor actions
+#
+#    elif choice == '2':
+#        logout()    # Logout
+#
+#        reset_password() ## added by Nantequiou
+#
+#        reset_password() ## added by Nantequiou
+#
+#def patient_actions():
+#    # Actions for the patient
+#    print("Your possible actions: ")
+#    print("\n1. View your medical record")
+#    print("\n3. Change password") ## added by Nantequiou
+#    
+#
+#    print()
+#    
+#    choice = input("Choose an action: ")
+#    if choice == '1':
+#        print("Folder is empty")    # Show empty directory (no medical record)
+#        print()
+#        patient_actions()   # Return to patient actions
+#    elif choice == '2':
+#        logout()    # Logout
+#    
+#    elif choice == '3':
+#        reset_password() ## added by Nantequiou
+#
 
 def logout():
     print("Logged out successfully.")   # Success message after logout
--- user.py
+++ user.py
@@ -1,6 +1,3 @@
-import hashlib
-import hmac
-
 admins = {
     'K.Kolbek': {'password': 'admin123', 'role': 'admin'},
     'L.Fischer': {'password': 'admin123', 'role': 'admin'},
