795bddb102fc1956df705dc87f7265315c005e3b
Kostja18 <konstantin.kolbek@stud.uni-goettingen.de>
2025-01-30 14:22:50 +0100

    Änderung vor Match

--- test_main.py
+++ test_main.py
@@ -8,32 +8,33 @@ def main():
     #while True:
     action = input("1:login 2:reset password 3:exit: ").strip().lower()
     
-    if action == "3":
-        print("Goodbye!")
-        sys.exit()
-    elif action == "1":
-        user = input("Username: ").strip()
-        pw = getpass.getpass("Password: ")
-        auth.login(user, pw)
+    try:
+        if action == "3":
+            print("Goodbye!")
+            sys.exit()
+        elif action == "1":
+            user = input("Username: ").strip()
+            pw = getpass.getpass("Password: ")
+            auth.login(user, pw)
 
-        while True:
-            user_role = auth.users[user].role
-            if user_role == 'doctor':
-                Actions.doktor_actions()
-            elif user_role == 'patient':
-                Actions.patient_actions()
-            elif user_role == 'admin': 
-                Actions.admin_actions()
-            else:
-                print("Error 404\n")
-            break
-
-            
-    elif action == "2":
-        auth.reset_password()
-    else:
-        print("\nInvalid input. Please try again.\n")
-        main()
+            while True:
+                user_role = auth.users[user].role
+                if user_role == 'doctor':
+                    Actions.doktor_actions()
+                elif user_role == 'patient':
+                    Actions.patient_actions()
+                elif user_role == 'admin': 
+                    Actions.admin_actions()
+                else:
+                    print("Error 404\n")
+                break         
+        elif action == "2":
+            auth.reset_password()
+        else:
+            print("\nInvalid input. Please try again.\n")
+            main()
+    except ValueError as error:
+            print(error)
 
 
     
