1e483c9fe4543bf84bc77f634108b05db7c96229
Kostja18 <konstantin.kolbek@stud.uni-goettingen.de>
2025-02-05 13:21:55 +0100

    update analyse

--- test_test.py
+++ test_test.py
@@ -4,14 +4,35 @@ import pandas as pd
 def main():
     analyse = Analyse()
     try:
-        df = pd.read_csv(analyse.omr_csv_path)
-        print(df.head(10))
+        test = input("Enter the name of the file you want to read: ")
+        if test == "omr":
+            print(analyse.read_omr())
+        elif test == "admissions":
+            print(analyse.read_admissions().head(10))
+        elif test == "diagnoses_icd":
+            print(analyse.read_diagnoses_icd().head(10))
+        elif test == "drgcodes":
+            print(analyse.read_drgcodes().head(10))
+        elif test == "emar":
+            print(analyse.read_emar().head(10))
+        elif test == "patients":
+            print(analyse.read_patients().head(10))
+        elif test == "pharmacy":
+            print(analyse.read_pharmacy().head(10))
+        elif test == "procedures_icd":
+            print(analyse.read_procedures_icd().head(10))
+        elif test == "d_icd_diagnoses":
+            print(analyse.read_d_icd_diagnoses().head(10))
+        elif test == "d_icd_procedures":
+            print(analyse.read_d_icd_procedures().head(10))
+        else:
+            print("Invalid input")
     except FileNotFoundError:
-        print(f"File not found: {analyse.omr_csv_path}")
+        print(f"File not found: {test}")
     except pd.errors.EmptyDataError:
-        print(f"No data: {analyse.omr_csv_path}")
+        print(f"No data: {test}")
     except pd.errors.ParserError:
-        print(f"Parse error: {analyse.omr_csv_path}")
+        print(f"Parse error: {test}")
     except Exception as e:
         print(f"An error occurred: {e}")
 
