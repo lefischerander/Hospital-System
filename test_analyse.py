12d6456eccc52a901ab145c3189cf6eee983b780
Kostja18 <konstantin.kolbek@stud.uni-goettingen.de>
2025-02-05 10:43:03 +0100

    Update test_analyse.py

--- test_analyse.py
+++ test_analyse.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os
+import pyodbc
+from warnings import filterwarnings
 
 class Analyse:
     def __init__(self):
@@ -16,5 +18,9 @@ class Analyse:
         self.d_icd_procedures_csv_path = os.path.join(self.current_dir, "Datenbank", "mimic", "mimic-demo", "hosp", "d_icd_procedures.csv.gz")
     
     #def read_csv(self, csv_path):
-        
-        
+
+filterwarnings("ignore", category=UserWarning, message='.*pandas only supports SQLAlchemy connectable.*')        
+conn_string = 'DRIVER={ODBC Driver 18 for SQL Server};SERVER=LAPTOP-CC0D63;DATABASE=LANK;UID=LANK_USER;PWD=Lank1.;TrustServerCertificate=YES'
+conn = pyodbc.connect(conn_string)
+sql = "SELECT * FROM omr"
+pd.read_sql(sql, conn)
