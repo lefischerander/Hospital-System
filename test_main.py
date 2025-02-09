79b16f0f47136b269a39511f5fdc29d3e9962dac
lefischerander <leander.fischer@icloud.com>
2025-02-09 23:24:04 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,13 +1,15 @@
 import getpass
-from test_class_login import AuthSystem
 import sys
+
 from test_class_actions import Actions
+from test_class_login import AuthSystem
 from user_test import User
+
 
 def main():
     auth = AuthSystem()
     action = input("1:login 2:reset password 3:exit: ").strip()
-    
+
     try:
         if action == "3":
             print("Goodbye!")
@@ -17,20 +19,20 @@ def main():
             pw = getpass.getpass("Password: ")
             pw = User.hash_password(pw)
             auth.login(user, pw)
-            if auth.logged_in == False:
+            if not auth.logged_in:
                 main()
-            while auth.logged_in == True:
+            while auth.logged_in:
                 user_role = auth.users[0][2]
                 subject_id = user
-                if user_role == 'Doctor':
+                if user_role == "Doctor":
                     Actions.doktor_actions()
-                elif user_role == 'Patient':
+                elif user_role == "Patient":
                     Actions.patient_actions()
-                elif user_role == 'admin': 
+                elif user_role == "admin":
                     Actions.admin_actions(subject_id)
                 else:
                     print("Error 404\n")
-                    break         
+                    break
         elif action == "2":
             user = input("Username: ").strip()
             pw = getpass.getpass("Your currend Password: ")
@@ -41,11 +43,8 @@ def main():
             print("\nInvalid input. Please try again.\n")
             main()
     except ValueError as error:
-            print(error)
+        print(error)
 
-
-
-    
 
 if __name__ == "__main__":
     main()
