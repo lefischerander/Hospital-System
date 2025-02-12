617c390859d8a8ccb0f7dc5de8014ed09bf7c7d8
lefischerander <leander.fischer@icloud.com>
2025-02-12 15:12:46 +0100

    Added variables for database information

--- Service_Database.py
+++ Service_Database.py
@@ -1,7 +1,11 @@
 import pyodbc
 
-
-connection_string = "DRIVER={ODBC Driver 18 for SQL Server};SERVER=LAPTOP-CC0D63;DATABASE=LANK;UID=LANK_USER;PWD=Lank1.;TrustServerCertificate=YES"
+# database information for the connection string
+MACHINE = "LAPTOP-CC0D63"
+DB = "LANK"
+USER = "LANK_USER"
+PASSWORD = "Lank1."
+connection_string = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={MACHINE};DATABASE={DB};UID={USER};PWD={PASSWORD};TrustServerCertificate=YES"
 
 # database tables
 LOGIN_DATA = "New_login_data"
