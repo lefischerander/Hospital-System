d32c87d0a5b4fbc9864f712072be13916a667d12
lefischerander <89662953+lefischerander@users.noreply.github.com>
2025-02-14 18:30:33 +0100

    trying to fix the gitignore issues

--- Database_Information.py
+++
@@ -1,7 +0,0 @@
-# database information for the connection string
-MACHINE = "LAPTOP-CC0D63"
-DB = "LANK"
-USER = "LANK_USER"
-PASSWORD = "Lank1."
-
-connection_string = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={MACHINE};DATABASE={DB};UID={USER};PWD={PASSWORD};TrustServerCertificate=YES"
--- Service_Database.py
+++ Service_Database.py
@@ -1,6 +1,16 @@
 import pyodbc
 from Database_Information import connection_string
 
+# Nante db access
+# database information for the connection string
+# MACHINE = "LAPTOP-CC0D63"
+# DB = "LANK"
+# USER = "LANK_USER"
+# PASSWORD = "Lank1."
+
+# connection_string = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={MACHINE};DATABASE={DB};UID={USER};PWD={PASSWORD};TrustServerCertificate=YES"'
+
+
 # database tables
 LOGIN_DATA = "New_login_data"
 DOCTORS = "doctors"
