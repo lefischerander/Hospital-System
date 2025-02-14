ef97c407f3497772e2b89307b16f2d7a85e87d65
Minischfee <minischfee@gmail.com>
2025-02-14 11:56:28 +0100

    helppage

---
+++ helppage.py
@@ -0,0 +1,14 @@
+from tkinter import Tk, Button, messagebox, Label, Entry, Toplevel, Frame, LEFT, RIGHT
+
+class HelpPage:
+    def help_page():
+        help_window = Tk()
+        help_window.title("Help Page")
+        help_window.geometry("800x600")
+        
+        Label(help_window, text="Help Page", font=("Arial", 20)).pack(pady=10)
+        Label(help_window, text="Welcome to the Help Page", font=("Arial", 15)).pack(pady=10)
+        Label(help_window, text="This is the help page for the Hospital Management System", font=("Arial", 15)).pack(pady=10)
+        Label(help_window, text="If you need help, please contact the system administrator", font=("Arial", 15)).pack(pady=10)
+        Button(help_window, text="Close", font=("Arial", 15), command=help_window.destroy).pack(pady=10)
+        help_window.mainloop()
