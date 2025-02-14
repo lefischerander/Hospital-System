4a725fcec17a979562a70164df0e6e68e3e30f9a
lefischerander <89662953+lefischerander@users.noreply.github.com>
2025-02-14 17:07:05 +0100

    added personal db functionality + docstring

--- main_ui.py
+++ main_ui.py
@@ -1,6 +1,5 @@
-
 # from test_class_actions import Actions
-from tkinter import Tk, Button, messagebox, Label, Entry, Toplevel, Frame, LEFT, RIGHT
+from tkinter import Tk, Button, messagebox, Label, Entry, Toplevel, Frame, RIGHT
 from test_class_login import AuthSystem
 from user_test import User
 from actions_ui import ActionsUI
@@ -10,9 +9,9 @@ global_username = None
 global_password = None
 
 
-
 def home_action():
     root.deiconify()
+
 
 def login_action():
     global global_username, global_password
@@ -72,6 +71,7 @@ def login_action():
     cancel_button = Button(button_frame, text="Cancel", command=cancel_login)
     cancel_button.pack(side=RIGHT, padx=5)
 
+
 def reset_password_action():
     root.withdraw()
     auth = AuthSystem()
@@ -100,9 +100,7 @@ def reset_password_action():
         new_password = new_password_entry.get()
         confirm_new_password = confirm_new_password_entry.get()
         hash_password = User.hash_password(old_password)
-        auth.reset_password(
-            username, hash_password, new_password, confirm_new_password
-        )
+        auth.reset_password(username, hash_password, new_password, confirm_new_password)
         messagebox.showinfo(
             "Reset Password Info", f"Username: {username}\nPassword: {old_password}"
         )
@@ -116,15 +114,12 @@ def reset_password_action():
     button_frame = Frame(reset_password_window)
     button_frame.pack(pady=10)
 
-    submit_button = Button(
-        button_frame, text="Submit", command=submit_reset_password
-    )
+    submit_button = Button(button_frame, text="Submit", command=submit_reset_password)
     submit_button.pack(side=RIGHT, padx=5)
 
-    cancel_button = Button(
-        button_frame, text="Cancel", command=cancel_reset_password
-    )
+    cancel_button = Button(button_frame, text="Cancel", command=cancel_reset_password)
     cancel_button.pack(side=RIGHT, padx=5)
+
 
 def exit_action():
     root.quit()
@@ -137,7 +132,9 @@ root.geometry("800x600")
 login_button = Button(root, text="Login", command=login_action)
 login_button.pack(pady=10)
 
-reset_password_button = Button(root, text="Reset Password", command=reset_password_action)
+reset_password_button = Button(
+    root, text="Reset Password", command=reset_password_action
+)
 reset_password_button.pack(pady=10)
 
 exit_button = Button(root, text="Exit", command=exit_action)
