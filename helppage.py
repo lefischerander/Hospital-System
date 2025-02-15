b264c6c5746abf8c08b4304b307c512c8f9bad0d
Minischfee <minischfee@gmail.com>
2025-02-15 17:26:36 +0100

    timeout test

--- helppage.py
+++ helppage.py
@@ -1,4 +1,6 @@
 from tkinter import Tk, Button, messagebox, Label, Entry, Toplevel, Frame, LEFT, RIGHT
+import tkinter.ttk as ttk
+import sys
 
 class HelpPage:
     def help_page():
@@ -11,4 +13,5 @@ class HelpPage:
         Label(help_window, text="This is the help page for the Hospital Management System", font=("Arial", 15)).pack(pady=10)
         Label(help_window, text="If you need help, please contact the system administrator", font=("Arial", 15)).pack(pady=10)
         Button(help_window, text="Close", font=("Arial", 15), command=help_window.destroy).pack(pady=10)
+        help_window.after(10000, sys.exit)
         help_window.mainloop()
