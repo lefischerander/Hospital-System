4ae0d2895a6ccc33fb2d3e372c5a0e48c5765eb8
Kostja18 <konstantin.kolbek@stud.uni-goettingen.de>
2025-02-15 15:44:01 +0100

    Update test_analyse.py

--- test_analyse.py
+++ test_analyse.py
@@ -7,21 +7,30 @@ from db_access import connection_string
 
 
 class Analyse:
+    """This class is used to analyse the data in the database."""
+
     def __init__(self):
+        """This method is used to initialize the connection_url and the User_service object."""
         self.connection_url = sa.URL.create(
             "mssql+pyodbc", query={"odbc_connect": connection_string}
         )
         self.us = db.User_service()
 
     def read_omr(self):
+        """This method is used to read the omr table and plot the data of the patient with the entered subject_id as line plot
+        and return the data as a string.
+
+        Returns:
+            str: The data of the patient with the entered subject_id.
+        """
         df = self.us.read_table_sa("omr")
 
-        id = int(input("Enter subject_id: "))  # Ask the user to enter the subject_id
-        patient = df[
-            df["subject_id"] == id
-        ]  # Get the patient with the entered subject_id
+        # Ask to enter the subject_id and sort the data by chartdate
+        id = int(input("Enter subject_id: "))
+        patient = df[df["subject_id"] == id]
         patient = patient.sort_values(by="chartdate")
 
+        # Initialize the variables
         x1 = x2 = x3 = x4 = None
 
         x1 = patient[patient["result_name"] == "Height (Inches)"]
@@ -29,6 +38,7 @@ class Analyse:
         x3 = patient[patient["result_name"] == "BMI (kg/m2)"]
         x4 = patient[patient["result_name"] == "Blood Pressure"]
 
+        # Plot the data
         fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 6))
 
         axes[0, 0].plot(x1["chartdate"], pd.to_numeric(x1["result_value"]))
@@ -49,12 +59,20 @@ class Analyse:
 
         plt.tight_layout()
         plt.show()
+
+        # Return the data of the patient as a string for better visualization
         patient_value = df[df["subject_id"] == id][
             ["chartdate", "result_name", "result_value"]
         ]
         return patient_value.to_string(index=False)
 
     def read_admissions(self):
+        """This method is used to read the admissions table and plot the duration of stay of all patients in the hospital as boxplot.
+        It also returns the data of the patient with the entered subject_id.
+
+        Returns:
+            str: The data of the patient with the entered subject_id.
+        """
         engine = sa.create_engine(self.connection_url)
 
         with engine.begin() as conn:
@@ -65,16 +83,19 @@ class Analyse:
                 conn,
             )
 
+        # Read the data from the admissions table and calculate the duration of stay in the hospital
         adtime = df["admittime"]
         ditime = df["dischtime"]
         gender = df["gender"]
         time = pd.to_datetime(ditime) - pd.to_datetime(adtime)
         days = time.dt.days
 
+        # Initialize the gender for the boxplot
         data_1 = days[gender == "M"]
         data_2 = days[gender == "F"]
         data = [data_1, data_2]
 
+        # Plot the data of all patients as boxplot
         plt.boxplot(
             data,
             patch_artist=True,
@@ -86,9 +107,10 @@ class Analyse:
         plt.ylabel("Days")
         plt.yticks(np.arange(0, max(days) + 1, 2))
         plt.grid(True, linestyle="--", linewidth=1.5, alpha=0.7)
-        plt.show()  # Display the plot
+        plt.show()
 
-        id = int(input("Enter subject_id: "))  # Ask the user to enter the subject_id
+        # Return the data of the patient as a string for better visualization
+        id = int(input("Enter subject_id: "))
         patient = df[df["subject_id"] == id][
             [
                 "admittime",
@@ -104,39 +126,65 @@ class Analyse:
         return patient.to_string(index=False)
 
     def read_diagnoses_icd(self):
+        """This method is used to read the diagnoses_icd table and return diagnoses of the patient with the entered subject_id.
+
+        Returns:
+            str: The diagnoses of the patient with the entered subject_id.
+        """
         df = self.us.read_table_sa("diagnoses_icd")
         df1 = self.us.read_table_sa("d_icd_diagnoses")
         merged_df = pd.merge(
             df, df1[["icd_code", "long_title"]], on="icd_code", how="left"
         )
-        id = int(input("Enter subject_id: "))  # Ask the user to enter the subject_id
+        id = int(input("Enter subject_id: "))
+        # Return the diagnoses of the patient as a string for better visualization
         patient = merged_df[merged_df["subject_id"] == id][
             ["seq_num", "icd_code", "icd_version", "long_title"]
         ]
         return patient.to_string(index=False)
 
     def read_drgcodes(self):
+        """This method is used to read the drgcodes table and return the drg codes of the patient with the entered subject_id.
+
+        Returns:
+            str: The drg codes of the patient with the entered subject_id.
+        """
         df = self.us.read_table_sa("drgcodes")
-        id = int(input("Enter subject_id: "))  # Ask the user to enter the subject_id
+        id = int(input("Enter subject_id: "))
+        # Return the drg codes of the patient as a string for better visualization
         patient = df[df["subject_id"] == id][
             ["drg_code", "description", "drg_severity", "drg_mortality"]
         ]
         return patient.to_string(index=False)
 
     def read_emar(self):
+        """This method is used to read the emar table and return the medication of the patient with the entered subject_id.
+
+        Returns:
+            str: The medication of the patient with the entered subject_id.
+        """
         df = self.us.read_table_sa("emar")
-        id = int(input("Enter subject_id: "))  # Ask the user to enter the subject_id
+        id = int(input("Enter subject_id: "))
+        # Return the medication of the patient as a string for better visualization
         patient = df[df["subject_id"] == id][
             ["pharmacy_id", "medication", "charttime", "scheduletime", "event_txt"]
         ]
         return patient.to_string(index=False)
 
     def read_patients(self):
+        """This method is used to read the patients table and plot the distribution of patients by age
+        and return the data of the patient with the entered subject_id.
+
+        Returns:
+            str: The data of the patient with the entered subject_id.
+        """
         df = self.us.read_table_sa("patients")
+        # Initialize the variables for the plot
         age = df["anchor_age"]
         gender = df["gender"]
         b_width = 5  # set the width of bin
 
+        # Plot the distribution of patients by age
         fig, ax = plt.subplots(figsize=(10, 6))  # Create a figure and axis
         ax.hist(
             age[gender == "M"],
@@ -158,14 +206,22 @@ class Analyse:
         ax.set_ylabel("Number of Patients")
         ax.legend(loc="upper right")
 
-        plt.show()  # Display the plot
-        id = int(input("Enter subject_id: "))  # Ask the user to enter the subject_id
+        plt.show()
+
+        id = int(input("Enter subject_id: "))
+        # Return the data of the patient as a string for better visualization
         patient = df[df["subject_id"] == id][["gender", "anchor_age", "dod"]]
         return patient.to_string(index=False)
 
     def read_pharmacy(self):
+        """This method is used to read the pharmacy table and return the pharmacy data of the patient with the entered subject_id.
+
+        Returns:
+            str: The pharmacy data of the patient with the entered subject_id.
+        """
         df = self.us.read_table_sa("pharmacy")
-        id = int(input("Enter subject_id: "))  # Ask the user to enter the subject_id
+        id = int(input("Enter subject_id: "))
+        # Return the pharmacy data of the patient as a string for better visualization
         patient = df[df["subject_id"] == id][
             [
                 "pharmacy_id",
@@ -179,29 +235,44 @@ class Analyse:
         return patient.to_string(index=False)
 
     def read_procedures_icd(self):
+        """This method is used to read the procedures_icd table and return the procedures of the patient with the entered subject_id.
+
+        Returns:
+            str: The procedures of the patient with the entered subject_id.
+        """
         df = self.us.read_table_sa("procedures_icd")
         df1 = self.us.read_table_sa("d_icd_procedures")
+        # Merge the data of the procedures_icd and d_icd_procedures table
         merged_df = pd.merge(
             df, df1[["icd_code", "long_title"]], on="icd_code", how="left"
         )
-        id = int(input("Enter subject_id: "))  # Ask the user to enter the subject_id
+        id = int(input("Enter subject_id: "))
+        # Return the procedures of the patient as a string for better visualization
         patient = merged_df[merged_df["subject_id"] == id][
             ["chartdate", "seq_num", "icd_code", "long_title", "icd_version"]
         ]
         return patient.to_string(index=False)
 
     def read_d_icd_diagnoses(self):
+        """This method is used to read the d_icd_diagnoses table and return the long title of the icd code entered by the user.
+
+        Returns:
+            str: The long title of the icd code entered by the user.
+        """
         df = self.us.read_table_sa("d_icd_diagnoses")
-        icd = input("Enter icd code: ")  # Ask the user to enter the icd code
-        icd_code = df[df["icd_code"].astype(str) == icd]["long_title"].values[
-            0
-        ]  # Get the long title of the icd code
+        icd = input("Enter icd code: ")
+        # Get the long title of the icd code
+        icd_code = df[df["icd_code"].astype(str) == icd]["long_title"]
         return icd_code
 
     def read_d_icd_procedures(self):
+        """This method is used to read the d_icd_procedures table and return the long title of the icd code entered by the user.
+
+        Returns:
+            str: The long title of the icd code entered by the user.
+        """
         df = self.us.read_table_sa("d_icd_procedures")
-        icd = input("Enter icd code: ")  # Ask the user to enter the icd code
-        icd_code = df[df["icd_code"].astype(str) == icd]["long_title"].values[
-            0
-        ]  # Get the long title of the icd code
+        icd = input("Enter icd code: ")
+        # Get the long title of the icd code
+        icd_code = df[df["icd_code"].astype(str) == icd]["long_title"]
         return icd_code
